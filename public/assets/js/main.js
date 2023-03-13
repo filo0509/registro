97052ec75c573ca39234f4a18f387f93bdf10e74
filo0509 <filippo.donati@liceodongnocchi.eu>
2023-03-13 22:59:21 +0100

    added dark mode

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,34 @@
-const loaderContainer = document.querySelector('.loader-container');
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        $("#spinner-container").fadeOut(400, 'swing', function () {
-            $(".spinner").remove()
-        });
-    }, 200)
+const loaderContainer = document.querySelector(".loader-container");
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    $("#spinner-container").fadeOut(400, "swing", function () {
+      $(".spinner").remove();
+    });
+  }, 200);
 });
+
+const modeBtn = document.getElementById("mode");
+modeBtn.onchange = (a) => {
+  if (modeBtn.checked === true) {
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.add("dark");
+    window.localStorage.setItem("mode", "dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add("light");
+    window.localStorage.setItem("mode", "light");
+  }
+};
+
+const mode = window.localStorage.getItem("mode");
+if (mode == "dark") {
+  modeBtn.checked = true;
+  document.documentElement.classList.remove("light");
+  document.documentElement.classList.add("dark");
+}
+
+if (mode == "light") {
+  modeBtn.checked = false;
+  document.documentElement.classList.remove("dark");
+  document.documentElement.classList.add("light");
+}
